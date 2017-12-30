09f23aca431c906a2debfc1f7cdc586d534f5814
ArianZargaran <arianzf@gmail.com>
2017-12-29 17:33:40 -0800

    Starting Youtube App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const API_KEY = 'AIzaSyDYfJganjjhbCOgTDS6MgSrufosMJmKidM';
+
+
+ReactDOM.render(<App />, document.querySelector('.container'))
