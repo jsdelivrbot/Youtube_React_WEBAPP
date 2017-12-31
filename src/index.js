2d11c4ab84d81b2940d6e3e0c38227436325cf78
ArianZargaran <arianzf@gmail.com>
2017-12-30 23:52:08 -0800

    API data imported

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/App';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDYfJganjjhbCOgTDS6MgSrufosMJmKidM';
 
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+  
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'))
